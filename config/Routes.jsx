a438d4e685ac0d33c620399221b263b268347397
Erik Jönsson <efjonsson@gmail.com>
2016-11-01 15:07:09 -0400

    remove main component

--- config/Routes.jsx
+++ config/Routes.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 import App from '../components/App.jsx';
-import Main from '../components/Main.jsx';
 import UserForm from '../components/Auth/UserForm.jsx';
 import NotFound from '../components/NotFound.jsx';
 
 const Routes = () => (
   <Router history={hashHistory} >
-    <Route name="app" path="/" component={App} >
-      <IndexRoute name="main" component={Main} />
-    </Route>
+    <Route name="app" path="/" component={App} />
     <Route name="login" path="/login" component={UserForm} />
     <Route name="signup" path="/signup" component={UserForm} />
     <Route name="not-found" path="*" component={NotFound} />
