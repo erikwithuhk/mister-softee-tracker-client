cdd6f4793132479d2ef376e60dbcc9ed7ffabedb
Erik Jönsson <efjonsson@gmail.com>
2016-10-31 21:08:12 -0400

    move routes

--- index.jsx
+++ index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './Routes.jsx';
+import Routes from './config/Routes.jsx';
 
 ReactDOM.render(<Routes />, document.querySelector('.root'));
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
         },
       },
     ],
