5b8563842594fa6fa4baf7f53a4d19db83a1346f
Erik Jönsson <efjonsson@gmail.com>
2016-11-02 23:51:25 -0400

    remove duplicate map files

--- components/map.jsx
+++ components/map.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
 import { Icon } from 'react-fa';
 
 const propTypes = {
-  containerElementProps: React.PropTypes.object,
+  // containerElementProps: React.PropTypes.object,
 };
 
 class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      mapRendered: false,
+      position: null,
+      vendorPositions: [
+        { lat: 40.671, lng: -73.962 },
+        { lat: 40.681, lng: -73.952 },
+        { lat: 40.631, lng: -73.982 },
+        { lat: 40.651, lng: -73.992 },
+      ],
     };
   }
   componentDidMount() {
@@ -34,68 +40,108 @@ class Map extends Component {
   clearPositionInterval() {
     clearInterval(this.state.userPositionIntervalID);
   }
+  initializeMap() {
+    this.map = new google.maps.Map(document.querySelector('.map'), {
+      zoom: 13,
+      center: this.state.position,
+    });
+    this.createCurrentLocationMarker();
+    this.createVendorLocationMarkers();
+    this.setState({ mapRendered: true });
+  }
+  createCurrentLocationMarker() {
+    new google.maps.Marker({
+      map: this.map,
+      position: this.state.position,
+      anchorPoint: new google.maps.Point(0, 0),
+      icon: {
+        url: '../images/mister-softee-tracker_current-location-dot.svg',
+        scaledSize: new google.maps.Size(64, 64),
+      },
+      opacity: 0.75,
+    });
+  }
+  createVendorLocationMarkers() {
+    this.state.vendorPositions.forEach((vendorPosition) => {
+      new google.maps.Marker({
+        map: this.map,
+        position: vendorPosition,
+        anchorPoint: new google.maps.Point(0, 0),
+        icon: {
+          url: '../images/mister-softee-tracker_truck-icon.svg',
+          scaledSize: new google.maps.Size(50, 32),
+        },
+      });
+    });
+  }
+  recenterMap() {
+    this.map.setCenter(this.state.position);
+  }
   render() {
     if (!this.state.position) {
       return (
-        <div className="map map--loading">
+        <section className="map map--loading">
           <p>Getting your location</p>
           <Icon pulse name="spinner" size="5x" />
-        </div>
+        </section>
       );
+    } else if (!this.state.mapRendered) {
+      this.initializeMap();
     }
     return (
-      <GoogleMapLoader
-        containerElement={
-          <section
-            className="map"
-            {...this.props.containerElementProps}
-          />
-        }
-        googleMapElement={
-          <GoogleMap
-            defaultZoom={12}
-            defaultCenter={this.state.position}
-          >
-            <Marker
-              anchorPoint={new google.maps.Point(50, 32)}
-              icon={{
-                url: '../images/mister-softee-tracker_current-location-dot.svg',
-                scaledSize: new google.maps.Size(64, 64),
-              }}
-              opacity={0.75}
-              position={this.state.position}
-            />
-            <Marker
-              position={{ lat: 40.671, lng: -73.962 }}
-              icon={{
-                url: '../images/mister-softee-tracker_truck-icon.svg',
-                scaledSize: new google.maps.Size(50, 32),
-              }}
-            />
-            <Marker
-              position={{ lat: 40.681, lng: -73.952 }}
-              icon={{
-                url: '../images/mister-softee-tracker_truck-icon.svg',
-                scaledSize: new google.maps.Size(50, 32),
-              }}
-            />
-            <Marker
-              position={{ lat: 40.631, lng: -73.982 }}
-              icon={{
-                url: '../images/mister-softee-tracker_truck-icon.svg',
-                scaledSize: new google.maps.Size(50, 32),
-              }}
-            />
-            <Marker
-              position={{ lat: 40.651, lng: -73.992 }}
-              icon={{
-                url: '../images/mister-softee-tracker_truck-icon.svg',
-                scaledSize: new google.maps.Size(50, 32),
-              }}
-            />
-          </GoogleMap>
-        }
-      />
+      <section className="map" />
+      // <GoogleMapLoader
+      //   containerElement={
+      //     <section
+      //       className="map"
+      //       {...this.props.containerElementProps}
+      //     />
+      //   }
+      //   googleMapElement={
+      //     <GoogleMap
+      //       defaultZoom={12}
+      //       defaultCenter={this.state.position}
+      //     >
+      //       <Marker
+      //         anchorPoint={new google.maps.Point(50, 32)}
+      //         icon={{
+      //           url: '../images/mister-softee-tracker_current-location-dot.svg',
+      //           scaledSize: new google.maps.Size(64, 64),
+      //         }}
+      //         opacity={0.75}
+      //         position={this.state.position}
+      //       />
+      //       <Marker
+      //         position={{ lat: 40.671, lng: -73.962 }}
+      //         icon={{
+      //           url: '../images/mister-softee-tracker_truck-icon.svg',
+      //           scaledSize: new google.maps.Size(50, 32),
+      //         }}
+      //       />
+      //       <Marker
+      //         position={{ lat: 40.681, lng: -73.952 }}
+      //         icon={{
+      //           url: '../images/mister-softee-tracker_truck-icon.svg',
+      //           scaledSize: new google.maps.Size(50, 32),
+      //         }}
+      //       />
+      //       <Marker
+      //         position={{ lat: 40.631, lng: -73.982 }}
+      //         icon={{
+      //           url: '../images/mister-softee-tracker_truck-icon.svg',
+      //           scaledSize: new google.maps.Size(50, 32),
+      //         }}
+      //       />
+      //       <Marker
+      //         position={{ lat: 40.651, lng: -73.992 }}
+      //         icon={{
+      //           url: '../images/mister-softee-tracker_truck-icon.svg',
+      //           scaledSize: new google.maps.Size(50, 32),
+      //         }}
+      //       />
+      //     </GoogleMap>
+      //   }
+      // />
     );
   }
 }
--- config/Routes.jsx
+++ config/Routes.jsx
@@ -3,7 +3,6 @@ import { Router, IndexRoute, Route, hashHistory } from 'react-router';
 import App from '../components/App.jsx';
 import Account from '../components/Account.jsx';
 import Map from '../components/Map.jsx';
-import Map2 from '../components/Map2.jsx';
 import UserForm from '../components/Auth/UserForm.jsx';
 import NotFound from '../components/NotFound.jsx';
 
@@ -11,7 +10,6 @@ const Routes = () => (
   <Router history={hashHistory} >
     <Route name="app" path="/" component={App} >
       <IndexRoute name="map" component={Map} />
-      <Route name="map2" path="/map2" component={Map2} />
       <Route name="request" path="request" component={Map} />
       <Route name="account" path="account" component={Account} />
     </Route>
